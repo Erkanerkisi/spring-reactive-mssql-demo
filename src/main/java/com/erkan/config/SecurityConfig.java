8a209aa7898788bd702a1b338a596515ea71b075
Erkan <erkanerkisi>
2021-12-18 16:32:18 +0300

    basic auth added

---
+++ src/main/java/com/erkan/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.erkan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/").permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin().disable()
+                .build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("$2a$04$0m9ETrzqyYf3HkWPabH1lOkxfgbI7vcNeQwcr5HN6jVF/BgDUxnG6")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+}
